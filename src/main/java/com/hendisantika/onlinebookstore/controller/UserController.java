557c27f2632be6cfbf7f7119651ed19a1f069d50
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:07:39 +0700

    #8 - Add UserController

---
+++ src/main/java/com/hendisantika/onlinebookstore/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.onlinebookstore.controller;
+
+import com.hendisantika.onlinebookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-book-store
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/05/20
+ * Time: 17.07
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+}
