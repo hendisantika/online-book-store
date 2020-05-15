129249ec37f6d99e0de80492c0813dae3bbfde79
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:10:34 +0700

    #16 - Add deleteBook API in BookController

--- src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
@@ -4,7 +4,9 @@ import com.hendisantika.onlinebookstore.model.Book;
 import com.hendisantika.onlinebookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +51,12 @@ public class BookController {
         book.setPicByte(this.bytes);
         bookRepository.save(book);
         this.bytes = null;
+    }
+
+    @DeleteMapping(path = {"/{id}"})
+    public Book deleteBook(@PathVariable("id") long id) {
+        Book book = bookRepository.getOne(id);
+        bookRepository.deleteById(id);
+        return book;
     }
 }
