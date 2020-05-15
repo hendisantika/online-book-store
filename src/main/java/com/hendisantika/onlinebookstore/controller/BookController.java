6846805086e5493e1831fb3bd4f97eb91e32950f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:10:52 +0700

    #17 - Add updateBook API in BookController

--- src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,5 +59,10 @@ public class BookController {
         Book book = bookRepository.getOne(id);
         bookRepository.deleteById(id);
         return book;
+    }
+
+    @PutMapping("/update")
+    public void updateBook(@RequestBody Book book) {
+        bookRepository.save(book);
     }
 }
