e6b8b8bc29758bcc506c0396f67e420bf47cd82a
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:09:41 +0700

    #13 - Add getBooks API in BookController

--- src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
@@ -1,10 +1,14 @@
 package com.hendisantika.onlinebookstore.controller;
 
+import com.hendisantika.onlinebookstore.model.Book;
 import com.hendisantika.onlinebookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +29,8 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    @GetMapping("/get")
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
 }
