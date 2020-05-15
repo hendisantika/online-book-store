eb94c6668ecd722576abf2b3485263df78af26a3
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:10:16 +0700

    #15 - Add createBook API in BookController

--- src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,12 @@ public class BookController {
     @PostMapping("/upload")
     public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
         this.bytes = file.getBytes();
+    }
+
+    @PostMapping("/add")
+    public void createBook(@RequestBody Book book) throws IOException {
+        book.setPicByte(this.bytes);
+        bookRepository.save(book);
+        this.bytes = null;
     }
 }
