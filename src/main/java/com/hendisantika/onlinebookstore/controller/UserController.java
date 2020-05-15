5e7260e30d0f800ff43f668e24724cd6629c80a5
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:08:28 +0700

    #10 - Add createUser API in UserController

--- src/main/java/com/hendisantika/onlinebookstore/controller/UserController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/UserController.java
@@ -5,6 +5,8 @@ import com.hendisantika.onlinebookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +33,10 @@ public class UserController {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    @PostMapping("/add")
+    public void createUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
 }
