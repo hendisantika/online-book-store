ee125f76f16122620bb660632c1ff0b4261220c0
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:10:00 +0700

    #14 - Add uploadImage API in BookController

--- src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/BookController.java
@@ -5,9 +5,13 @@ import com.hendisantika.onlinebookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -32,5 +36,10 @@ public class BookController {
     @GetMapping("/get")
     public List<Book> getBooks() {
         return bookRepository.findAll();
+    }
+
+    @PostMapping("/upload")
+    public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+        this.bytes = file.getBytes();
     }
 }
