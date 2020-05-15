8ce0498232dc45aa1b3f1a24ecbe4e0a9511e06f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:08:42 +0700

    #11 - Add deleteUser API in UserController

--- src/main/java/com/hendisantika/onlinebookstore/controller/UserController.java
+++ src/main/java/com/hendisantika/onlinebookstore/controller/UserController.java
@@ -4,7 +4,9 @@ import com.hendisantika.onlinebookstore.model.User;
 import com.hendisantika.onlinebookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,13 @@ public class UserController {
     public void createUser(@RequestBody User user) {
         userRepository.save(user);
     }
+
+    @DeleteMapping(path = {"/{id}"})
+    public User deleteUser(@PathVariable("id") long id) {
+        User user = userRepository.getOne(id);
+        userRepository.deleteById(id);
+        return user;
+    }
+
 
 }
