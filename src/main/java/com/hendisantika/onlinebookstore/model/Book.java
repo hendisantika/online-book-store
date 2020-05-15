b6b392d7059b39094cd632de19d69e352ecbef16
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-15 17:03:59 +0700

    #4 - Add Book Entity

---
+++ src/main/java/com/hendisantika/onlinebookstore/model/Book.java
@@ -0,0 +1,47 @@
+package com.hendisantika.onlinebookstore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-book-store
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/05/20
+ * Time: 17.01
+ */
+@Entity
+@Table(name = "book")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "picByte", length = 100000)
+    private byte[] picByte;
+
+}
