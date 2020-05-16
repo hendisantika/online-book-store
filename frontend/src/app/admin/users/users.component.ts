190e3824c95b8c1d36d6a08eb197a501c4a35b52
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 05:18:09 +0700

    #44 - Modify Refresh Data & Add  viewUser

--- frontend/src/app/admin/users/users.component.ts
+++ frontend/src/app/admin/users/users.component.ts
@@ -65,7 +65,15 @@ export class UsersComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe(
       (params) => {
         this.action = params['action']
+        const selectedUserId = params['id'];
+        if (selectedUserId) {
+          this.selectedUser = this.users.find(user => user.id === +selectedUserId);
+        }
       }
     );
+  }
+
+  viewUser(id: number) {
+    this.router.navigate(['admin', 'users'], {queryParams: {id, action: 'view'}});
   }
 }
