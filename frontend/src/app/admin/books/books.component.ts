809bab787d29e96ce68ac1e76596e55c96fa9754
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 05:34:01 +0700

    #54 - Init Data Book

--- frontend/src/app/admin/books/books.component.ts
+++ frontend/src/app/admin/books/books.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Book} from "../../model/book";
 
 @Component({
   selector: 'app-books',
@@ -7,10 +8,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
+  books: Array<Book>;
+
   constructor() {
   }
 
   ngOnInit() {
+
+    this.books = new Array<Book>();
+
+    const book1 = new Book();
+    book1.id = 1;
+    book1.name = 'book1';
+    book1.author = 'author1';
+    book1.price = 5;
+
+    const book2 = new Book();
+    book2.id = 2;
+    book2.name = 'book2';
+    book2.author = 'author2';
+    book2.price = 15;
+
+    this.books.push(book1);
+    this.books.push(book2);
   }
 
 }
