2010766ea5ead2d9e6704699f21c3366d7112e51
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 05:22:53 +0700

    #49 - Add constructor & add deleteUser service

--- frontend/src/app/admin/users/viewuser/viewuser.component.ts
+++ frontend/src/app/admin/users/viewuser/viewuser.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../../model/user";
+import {Router} from "@angular/router";
+import {HttpClientService} from "../../../service/http-client.service";
 
 @Component({
   selector: 'app-viewuser',
@@ -10,10 +12,18 @@ export class ViewuserComponent implements OnInit {
   @Input()
   user: User
 
-  constructor() {
+  constructor(private httpClientService: HttpClientService,
+              private router: Router) {
   }
 
   ngOnInit() {
   }
 
+  deleteUser() {
+    this.httpClientService.deleteUser(this.user.id).subscribe(
+      (user) => {
+        this.router.navigate(['admin', 'users']);
+      }
+    );
+  }
 }
