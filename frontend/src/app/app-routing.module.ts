86c2f9e5eebe46d53d11c2cc1b7154033615c23b
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-16 21:29:31 +0700

    #30 - Modify modifying the app routing to define the route for the user component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UsersComponent} from "./admin/users/users.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin/users', component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
