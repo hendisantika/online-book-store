b3049298a2e57a3f0435f89ac482ea2fbd88881f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-16 21:04:48 +0700

    #20 - Add frontend folder & source code

--- src/test/java/com/hendisantika/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/hendisantika/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -1,13 +1,25 @@
 package com.hendisantika.onlinebookstore;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OnlineBookStoreApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void replaceChar() {
+        String sentences = "wajahmu bagaikan rembulan";
+        System.out.println("sentences = " + sentences);
+        for (int i = 0; i < sentences.length(); i++) {
+            int asciiValue = sentences.charAt(i);
+            if (asciiValue == 32) {
+                System.out.print((char) (asciiValue));
+            } else {
+                System.out.print((char) (asciiValue + 1));
+            }
+        }
+    }
 }
