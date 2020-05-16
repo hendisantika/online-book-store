383eecb898da3069eb354d073b280f8b7e08888c
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 05:20:44 +0700

    #46 - Modify viewuser.component.ts

--- frontend/src/app/admin/users/viewuser/viewuser.component.ts
+++ frontend/src/app/admin/users/viewuser/viewuser.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../../model/user";
 
 @Component({
   selector: 'app-viewuser',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./viewuser.component.css']
 })
 export class ViewuserComponent implements OnInit {
+  @Input()
+  user: User
 
   constructor() {
   }
