acc7313f275dff64a279b504cbdf01f34036724b
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 04:46:44 +0700

    #38 - Modify the Users component admin/users/adduser

--- frontend/src/app/admin/users/adduser/adduser.component.ts
+++ frontend/src/app/admin/users/adduser/adduser.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../../model/user";
 
 @Component({
   selector: 'app-adduser',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./adduser.component.css']
 })
 export class AdduserComponent implements OnInit {
+  @Input()
+  user: User
 
   constructor() {
   }
