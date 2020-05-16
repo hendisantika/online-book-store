0d687855db903c491613201e77f6e003628d9a6e
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-16 21:27:13 +0700

    #28 - Modify users.component.ts

--- frontend/src/app/admin/users/users.component.ts
+++ frontend/src/app/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,28 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: Array<User>;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.users = new Array<User>();
+
+    const user1 = new User();
+    user1.id = 1;
+    user1.name = 'user1';
+    user1.type = 'admin';
+    user1.password = 'pass';
+
+    const user2 = new User();
+    user2.id = 2;
+    user2.name = 'user2';
+    user2.type = 'user';
+    user2.password = 'pass';
+
+    this.users.push(user1);
+    this.users.push(user2);
   }
 
 }
