9c0af5112d00c1630c44cb6cb32c11b847b23300
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-16 21:39:21 +0700

    #33 - Add the HTTPClientModule to the app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MenuComponent} from './menu/menu.component';
 import {UsersComponent} from './admin/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {UsersComponent} from './admin/users/users.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
