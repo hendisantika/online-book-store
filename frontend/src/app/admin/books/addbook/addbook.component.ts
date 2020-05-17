51a459674833b1c2fe786c34bb2691d651bdc372
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-18 05:07:59 +0700

    #86 - If there is no book id then it is an add book call else it is an edit book call

--- frontend/src/app/admin/books/addbook/addbook.component.ts
+++ frontend/src/app/admin/books/addbook/addbook.component.ts
@@ -40,25 +40,34 @@ export class AddbookComponent implements OnInit {
   }
 
   saveBook() {
+//If there is no book id then it is an add book call else it is an edit book call
+    if (this.book.id == null) {
+      const uploadData = new FormData();
+      uploadData.append('imageFile', this.selectedFile, this.selectedFile.name);
+      this.selectedFile.imageName = this.selectedFile.name;
 
-    const uploadData = new FormData();
-    uploadData.append('imageFile', this.selectedFile, this.selectedFile.name);
-    this.selectedFile.imageName = this.selectedFile.name;
-
-    this.httpClient.post('http://localhost:8080/books/upload', uploadData, {observe: 'response'})
-      .subscribe((response) => {
-          if (response.status === 200) {
-            this.httpClientService.addBook(this.book).subscribe(
-              (book) => {
-                this.bookAddedEvent.emit();
-                this.router.navigate(['admin', 'books']);
-              }
-            );
-            console.log('Image uploaded successfully');
-          } else {
-            console.log('Image not uploaded successfully');
+      this.httpClient.post('http://localhost:8080/books/upload', uploadData, {observe: 'response'})
+        .subscribe((response) => {
+            if (response.status === 200) {
+              this.httpClientService.addBook(this.book).subscribe(
+                (book) => {
+                  this.bookAddedEvent.emit();
+                  this.router.navigate(['admin', 'books']);
+                }
+              );
+              console.log('Image uploaded successfully');
+            } else {
+              console.log('Image not uploaded successfully');
+            }
           }
+        );
+    } else {
+      this.httpClientService.updateBook(this.book).subscribe(
+        (book) => {
+          this.bookAddedEvent.emit();
+          this.router.navigate(['admin', 'books']);
         }
       );
+    }
   }
 }
