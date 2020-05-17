e5444790e0379c8e9ce97fd841ad1189b64ec820
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 07:04:57 +0700

    #60 - Modify Book component

--- frontend/src/app/admin/books/books.component.ts
+++ frontend/src/app/admin/books/books.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Book} from "../../model/book";
 import {HttpClientService} from "../../service/http-client.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-books',
@@ -10,8 +11,11 @@ import {HttpClientService} from "../../service/http-client.service";
 export class BooksComponent implements OnInit {
 
   books: Array<Book>;
+  selectedBook: Book;
+  action: string;
 
-  constructor(private httpClientService: HttpClientService) {
+  constructor(private httpClientService: HttpClientService, private activatedRoute: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -19,10 +23,21 @@ export class BooksComponent implements OnInit {
     this.httpClientService.getBooks().subscribe(
       response => this.handleSuccessfulResponse(response)
     );
+
+    this.activatedRoute.queryParams.subscribe(
+      (params) => {
+        this.action = params['action'];
+      }
+    );
   }
 
   handleSuccessfulResponse(response) {
     this.books = response;
+  }
+
+  addBook() {
+    this.selectedBook = new Book();
+    this.router.navigate(['admin', 'books'], {queryParams: {action: 'add'}});
   }
 
 }
