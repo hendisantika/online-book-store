1b2a08b362b5beb6b8b4527036fc3587a7a199fe
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 07:23:22 +0700

    #72 - Modify Book Component

--- frontend/src/app/admin/books/books.component.ts
+++ frontend/src/app/admin/books/books.component.ts
@@ -11,24 +11,17 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class BooksComponent implements OnInit {
 
   books: Array<Book>;
+  booksRecieved: Array<Book>;
   selectedBook: Book;
   action: string;
 
-  constructor(private httpClientService: HttpClientService, private activatedRoute: ActivatedRoute,
+  constructor(private httpClientService: HttpClientService,
+              private activatedRoute: ActivatedRoute,
               private router: Router) {
   }
 
   ngOnInit() {
-
-    this.httpClientService.getBooks().subscribe(
-      response => this.handleSuccessfulResponse(response)
-    );
-
-    this.activatedRoute.queryParams.subscribe(
-      (params) => {
-        this.action = params['action'];
-      }
-    );
+    this.refreshData();
   }
 
   refreshData() {
@@ -37,13 +30,40 @@ export class BooksComponent implements OnInit {
     );
     this.activatedRoute.queryParams.subscribe(
       (params) => {
+        // get the url parameter named action. this can either be add or view.
         this.action = params['action'];
+        // get the parameter id. this will be the id of the book whose details
+        // are to be displayed when action is view.
+        const id = params['id'];
+        // if id exists, convert it to integer and then retrive the book from
+        // the books array
+        if (id) {
+          this.selectedBook = this.books.find(book => {
+            return book.id === +id;
+          });
+        }
       }
     );
   }
 
+  //we will be taking the books response returned from the database
+  // and we will be adding the retrieved
   handleSuccessfulResponse(response) {
-    this.books = response;
+    this.books = new Array<Book>();
+    //get books returned by the api call
+    this.booksRecieved = response;
+    for (const book of this.booksRecieved) {
+
+      const bookwithRetrievedImageField = new Book();
+      bookwithRetrievedImageField.id = book.id;
+      bookwithRetrievedImageField.name = book.name;
+      //populate retrieved image field so that book image can be displayed
+      bookwithRetrievedImageField.retrievedImage = 'data:image/jpeg;base64,' + book.picByte;
+      bookwithRetrievedImageField.author = book.author;
+      bookwithRetrievedImageField.price = book.price;
+      bookwithRetrievedImageField.picByte = book.picByte;
+      this.books.push(bookwithRetrievedImageField);
+    }
   }
 
   addBook() {
@@ -51,4 +71,7 @@ export class BooksComponent implements OnInit {
     this.router.navigate(['admin', 'books'], {queryParams: {action: 'add'}});
   }
 
+  viewBook(id: number) {
+    this.router.navigate(['admin', 'books'], {queryParams: {id, action: 'view'}});
+  }
 }
