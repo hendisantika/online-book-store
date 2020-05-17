dcb88c314bdb45cc2214164884df450082c830b1
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-18 05:09:24 +0700

    #88 - Add a path for the shop book component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {UsersComponent} from "./admin/users/users.component";
 import {BooksComponent} from "./admin/books/books.component";
+import {ShopbookComponent} from "./shopbook/shopbook.component";
 
 const routes: Routes = [
   {path: 'admin/users', component: UsersComponent},
-  {path: 'admin/books', component: BooksComponent}
+  {path: 'admin/books', component: BooksComponent},
+  {path: 'shop', component: ShopbookComponent}
 ];
 
 @NgModule({
