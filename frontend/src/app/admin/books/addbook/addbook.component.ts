6f6e7206b4e30367aacb2f4126b3e1313233b78d
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 07:07:01 +0700

    #62 - Modify Book component

--- frontend/src/app/admin/books/addbook/addbook.component.ts
+++ frontend/src/app/admin/books/addbook/addbook.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Book} from "../../../model/book";
 
 @Component({
   selector: 'app-addbook',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./addbook.component.css']
 })
 export class AddbookComponent implements OnInit {
+  @Input()
+  book: Book;
 
   constructor() {
   }
