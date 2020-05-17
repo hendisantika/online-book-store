f4dfce0d2c64188723409ed8ab7ca0e103aa10f1
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-17 07:26:15 +0700

    #74 - Modify View Book Component

--- frontend/src/app/admin/books/viewbook/viewbook.component.ts
+++ frontend/src/app/admin/books/viewbook/viewbook.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Book} from "../../../model/book";
 
 @Component({
   selector: 'app-viewbook',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./viewbook.component.css']
 })
 export class ViewbookComponent implements OnInit {
+  @Input()
+  book: Book;
 
   constructor() {
   }
