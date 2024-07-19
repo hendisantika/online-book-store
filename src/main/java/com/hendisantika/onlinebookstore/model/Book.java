c56d8c335754c7928440a09b2654846baba9583c
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 16:30:47 +0700

    Bug Fix codes🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/onlinebookstore/model/Book.java
+++ src/main/java/com/hendisantika/onlinebookstore/model/Book.java
@@ -1,15 +1,14 @@
 package com.hendisantika.onlinebookstore.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 /**
  * Created by IntelliJ IDEA.
